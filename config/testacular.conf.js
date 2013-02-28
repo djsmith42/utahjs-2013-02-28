11e3222f50ea86ea10085c08f6ce90059155eff4
Dave Smith <dsmith@hirevue.com>
2013-02-28 10:49:04 -0700

    Testacular config

---
+++ config/testacular.conf.js
@@ -0,0 +1,11 @@
+basePath = '../'; // all relative paths in this file will be based on this (and this is based on the location of this file)
+
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  'static/*.js',
+  'static/my-directives.js',
+  'tests/*.js'
+];
+
+autoWatch = true; // re-run tests when they change on disk
