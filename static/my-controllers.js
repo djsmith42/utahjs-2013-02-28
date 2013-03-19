ebe7b63b7c2f4d9f75bbe3957bd43bcb20fd15b3
Dave Smith <dsmith@hirevue.com>
2013-03-19 16:40:25 -0600

    Add a few more directives to demo

--- static/my-controllers.js
+++ static/my-controllers.js
@@ -1,4 +1,4 @@
-function HomePageController($scope, $http) {
+function PersonDemoController($scope, $http) {
     $scope.userFirstName = "Fred";
     $http.get("/api/people").success(function(people) {
         $scope.people = people;
@@ -12,5 +12,43 @@ function HomePageController($scope, $http) {
 
     $scope.personClicked = function(person) {
         alert("A person got clicked: " + person.name);
+    }
+}
+
+
+
+function EatClickController($scope) {
+    $scope.linkClicked = function() {
+        alert("The link got clicked");
+    }
+}
+
+
+
+
+function ProgressBarDemoController($scope, $timeout) {
+    $scope.totalPuppies   = 20;
+    $scope.puppiesCuddled =  0;
+
+    $scope.totalSpoons  = 1000;
+    $scope.spoonsWashed =    0;
+
+    _incrementPuppiesCuddled();
+    _incrementSpoonsWashed();
+
+    function _incrementPuppiesCuddled() {
+        $scope.puppiesCuddled++;
+        if ($scope.puppiesCuddled > $scope.totalPuppies) {
+            $scope.puppiesCuddled = 0;
+        }
+        $timeout(_incrementPuppiesCuddled, 1000);
+    }
+
+    function _incrementSpoonsWashed() {
+        $scope.spoonsWashed++;
+        if ($scope.spoonsWashed > $scope.totalSpoons) {
+            $scope.spoonsWashed = 0;
+        }
+        $timeout(_incrementSpoonsWashed, 100);
     }
 }
--- static/my-directives.js
+++ static/my-directives.js
@@ -7,6 +7,66 @@ directive("myPersonWidget", function() {
         scope: {
             person: '=',
             clicked: '='
+        },
+        controller: function($scope) {
+            $scope.shouldShowButton = function() {
+                return $scope.person.age > 16;
+            }
         }
     }
-});
+}).
+directive("eatClick", function() {
+    return {
+        link: function(scope, element, attrs) {
+            element.click(function(event) {
+                console.log("Eating the click");
+                event.preventDefault();
+            });
+        }
+    }
+}).
+directive('throbbingText', function() {
+    return {
+        link: function(scope, element, attrs) {
+            var FadeMillis = 900;
+            var IntervalMillis = 100;
+            var _isFaded = false;
+            _doFade();
+            function _doFade() {
+                if (element.is(':visible')) {
+                    var fadeTo = _isFaded ? 0.9 : 0.4;
+                    element.fadeTo(FadeMillis, fadeTo, function() {
+                        _isFaded = !_isFaded;
+                    });
+                }
+                // Don't use $timeout because it causes all $scopes to re-$digest
+                setTimeout(_doFade, FadeMillis + IntervalMillis);
+            }
+        }
+    }
+}).
+directive('progressBar', function() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        replace: true,
+        scope: {
+            done  : '=', // number of "done" steps
+            total : '='  // total number of steps
+        },
+        template: '<div class="progress progress-success"><span class="bar"></span></div>',
+        controller: function($scope, $element) {
+            var bar = $element.find(".bar");
+            function _update() {
+                if ($scope.total > 0) {
+                    var percent = 100.0 * $scope.done / $scope.total;
+                } else {
+                    var percent = 0;
+                }
+                bar.width(percent + "%");
+            }
+            $scope.$watch("done",  _update);
+            $scope.$watch("total", _update);
+        }
+    }
+})
